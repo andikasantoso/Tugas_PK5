543170f251ef7fbc9f7cfc9206f9ac61515a8996
andikasantoso <30432160+andikasantoso@users.noreply.github.com>
2017-10-14 11:15:26 +0700

    Create hasil.php

---
+++ hasil.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+</head>
+<body>
+<table align="center">
+
+<form action="hasil.php" method="post">
+<h1><center><b> INSTAGRAM</center></b></h1>
+<hr width="20%">
+<tr>
+			<td><input type='Email' name='email' placeholder="Email"></input>
+	  </tr>
+<tr>
+			<td><input type='text' name='fullname' placeholder="Fullname"></input>
+	  </tr>
+<tr>
+			<td><input type='text' name='username' placeholder="Username"></input>
+	  </tr>
+<tr>
+			<td><input type='Password' name='password'placeholder="Password"></input>
+	  </tr>
+<tr>
+			<td><input type='submit' value='Sign Up' ></input>
+	  </tr>
+</form>
+</table>
+</body>
+</html>
